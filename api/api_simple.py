f42bd3650b2da37b7d5802989151d24807392df4
James Endicott <endicott.james@gmail.com>
2024-10-23 14:32:14 -0500

    Move list of levels earlier in the file so it's easier to find if we need to change it again.

--- api/api_simple.py
+++ api/api_simple.py
@@ -10,6 +10,8 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+Level = StrEnum("LEVELS", ["BACK", "BODY", "EYES", "HAT", "MOUTH", "BADGE", "ARMS", "FRONT"])
+
 def get_src_parts():
     if "src_parts" not in g:
         g.src_parts = {}
@@ -36,8 +38,6 @@ def make_specification_from_request(request):
             else:
                 specification[Level[k.upper()]] = v
     return specification
-
-Level = StrEnum("LEVELS", ["BODY", "EYES", "HAT", "MOUTH", "ARMS", "FRONT"])
 
 @dataclass
 class Layer:
